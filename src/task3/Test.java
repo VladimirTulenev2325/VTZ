f104fa577cd1291749c404ad5ea89713cbeefb6e
VladimirTulenev2325 <beatstours2325@yandex.ru>
2023-06-02 17:34:23 +0300

    t1

--- src/task3/Test.java
+++ src/task3/Test.java
@@ -1,8 +1,9 @@
 package task3;
 
-class Test {
-    public int id;
-    public String title;
-    public String value;
-    public Test[] values;
+ class Test {
+        public int id;
+        public String title;
+        public String value;
+        public Test[] values;
+    }
 }
